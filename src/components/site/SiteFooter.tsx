161da7536c1ddc8e2d4a7622ff1bb626049925cd
logeshOffical <iamlogeshwaran.info@gmail.com>
2026-07-04 16:53:16 +0530

    Added instagram link

--- src/components/site/SiteFooter.tsx
+++ src/components/site/SiteFooter.tsx
@@ -19,7 +19,7 @@ export function SiteFooter() {
             grandparents taught us.
           </p>
           <div className="flex gap-3 mt-6">
-            <a href="#" className="p-2 rounded-full bg-white/10 hover:bg-white/20 gentle-animation">
+            <a href="https://www.instagram.com/rvs_agri/" className="p-2 rounded-full bg-white/10 hover:bg-white/20 gentle-animation">
               <Instagram className="w-4 h-4" />
             </a>
             <a href="https://www.facebook.com/growingthefuturesince1914" className="p-2 rounded-full bg-white/10 hover:bg-white/20 gentle-animation">
